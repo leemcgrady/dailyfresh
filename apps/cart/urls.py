61db333d6fc1075bbfb15fdaadb7ae0ff31003b7
lige_ubuntu <1415957621@qq.com>
2018-11-13 21:51:03 +0800

    添加购物车

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,5 +1,12 @@
+from django.conf.urls import url
+# from django.contrib.auth.decorators import login_required
+from cart.views import CartAddView
+
 urlpatterns = [
 
+    url(r'^add$', CartAddView.as_view(), name='add'),  # 添加购物车记录
+
 ]
+
 
 # app_name = 'cart'
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import JsonResponse
+
+from goods.models import GoodsSKU
+from django_redis import get_redis_connection
+from utils.mixin import LoginRequiredMixin
 
 # Create your views here.
+
+
+class CartAddView(View):
+
+    def post(self, request):
+
+        user = request.user
+
+        if not user.is_authenticated:
+            return JsonResponse({"res": 0, "errmsg": "请先登录"})
+
+        sku_id = request.POST.get("sku_id")
+        count = request.POST.get("count")
+
+        if not all([sku_id, count]):
+            return JsonResponse({"res": 1, "errmsg": "数据不完整"})
+
+        try:
+            count = int(count)
+        except Exception as e:
+            return JsonResponse({"res": 2, "errmsg": "数量出错"})
+
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist:
+            return JsonResponse({"res": 3, "errmsg": "商品不存在"})
+
+        conn = get_redis_connection("default")
+
+        cart_key = "cart_%d" % user.id
+
+        cart_count = conn.hget(cart_key, sku_id)
+
+        if cart_count:
+            count = count + int(cart_count)
+
+        if count > sku.stock:
+            return JsonResponse({"res": 4, "errmsg": "商品库存不足"})
+
+        conn.hset(cart_key, sku_id, count)
+
+        total_count = conn.hlen(cart_key)
+
+        return JsonResponse({"res": 5, "total_count": total_count, "message": "添加成功"})
+
