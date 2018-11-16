cc01e919dbdef01d8719af0d74f6b463481d859c
lige_ubuntu <1415957621@qq.com>
2018-11-16 14:01:05 +0800

    创建订单

--- apps/order/views.py
+++ apps/order/views.py
@@ -66,19 +66,19 @@ class OrderPlaceView(LoginRequiredMixin, View):
 
         sku_ids = ",".join(sku_ids)
 
-        context = {'skus':skus,
-                   'total_count':total_count,
-                   'total_price':total_price,
-                   'transit_price':transit_price,
-                   'total_pay':total_pay,
-                   'addrs':addrs,
-                   'sku_ids':sku_ids}
+        context = {'skus': skus,
+                   'total_count': total_count,
+                   'total_price': total_price,
+                   'transit_price': transit_price,
+                   'total_pay': total_pay,
+                   'addrs': addrs,
+                   'sku_ids': sku_ids}
 
         # 使用模板
         return render(request, 'place_order.html', context)
 
 
-class OrderCommitView(View):
+class OrderCommitView1(View):
 
     @transaction.atomic
     def post(self, request):
@@ -89,7 +89,6 @@ class OrderCommitView(View):
 
             return JsonResponse({"res": 0, "errmsg": "请先登录"})
 
-
         # 接收参数
         addr_id = request.POST.get('addr_id')
         pay_method = request.POST.get('pay_method')
@@ -97,20 +96,20 @@ class OrderCommitView(View):
 
         # 校验参数
         if not all([addr_id, pay_method, sku_ids]):
-            return JsonResponse({'res':1, 'errmsg':'参数不完整'})
+            return JsonResponse({'res': 1, 'errmsg':'参数不完整'})
 
         # 校验支付方式
         if pay_method not in OrderInfo.PAY_METHODS.keys():
-            return JsonResponse({'res':2, 'errmsg':'非法的支付方式'})
+            return JsonResponse({'res': 2, 'errmsg': '非法的支付方式'})
 
         # 校验地址
         try:
             addr = Address.objects.get(id=addr_id)
         except Address.DoesNotExist:
             # 地址不存在
-            return JsonResponse({'res':3, 'errmsg':'地址非法'})
+            return JsonResponse({'res': 3, 'errmsg': '地址非法'})
 
-        order_id = datetime.now().strftime("%Y%h%m%H%M%S") + str(user.id)
+        order_id = datetime.now().strftime("%Y%m%d%H%M%S") + str(user.id)
 
         transit_price = 10
 
@@ -177,4 +176,99 @@ class OrderCommitView(View):
         return JsonResponse({'res':5, 'message':'创建成功'})
 
 
+class OrderCommitView(View):
 
+    @transaction.atomic
+    def post(self, request):
+
+        user = request.user
+
+        if not user.is_authenticated:
+
+            return JsonResponse({"res": 0, "errmsg": "请先登录"})
+
+        # 接收参数
+        addr_id = request.POST.get('addr_id')
+        pay_method = request.POST.get('pay_method')
+        sku_ids = request.POST.get('sku_ids') # 1,3
+
+        # 校验参数
+        if not all([addr_id, pay_method, sku_ids]):
+            return JsonResponse({'res': 1, 'errmsg':'参数不完整'})
+
+        # 校验支付方式
+        if pay_method not in OrderInfo.PAY_METHODS.keys():
+            return JsonResponse({'res': 2, 'errmsg': '非法的支付方式'})
+
+        # 校验地址
+        try:
+            addr = Address.objects.get(id=addr_id)
+        except Address.DoesNotExist:
+            # 地址不存在
+            return JsonResponse({'res': 3, 'errmsg': '地址非法'})
+
+        order_id = datetime.now().strftime("%Y%m%d%H%M%S") + str(user.id)
+
+        transit_price = 10
+
+        total_count = 0
+        total_price = 0
+
+        save_id = transaction.savepoint()
+
+        try:
+
+            order = OrderInfo.objects.create(order_id=order_id,
+                                             user=user,
+                                             addr=addr,
+                                             pay_method=pay_method,
+                                             total_count=total_count,
+                                             total_price=total_price,
+                                             transit_price=transit_price)
+
+            conn = get_redis_connection("default")
+
+            cart_key = "cart_%d" % user.id
+
+            sku_ids = sku_ids.split(",")
+
+            for sku_id in sku_ids:
+
+                try:
+                    sku = GoodsSKU.objects.get(id=sku_id)
+
+                except:
+                    transaction.savepoint_rollback(save_id)
+                    return JsonResponse({'res': 4, 'errmsg':'商品不存在'})
+
+                count = conn.hget(cart_key, sku_id)
+
+                if int(count) > sku.stock:
+                    transaction.savepoint_rollback(save_id)
+                    return JsonResponse({'res': 6, 'errmsg':'商品库存不足'})
+
+                OrderGoods.objects.create(order=order,
+                                          sku=sku,
+                                          count=count,
+                                          price=sku.price)
+                sku.stock -= 1
+                sku.sales += 1
+                sku.save()
+
+                amount = sku.price * int(count)
+                total_count += int(count)
+                total_price += amount
+
+            order.total_count = total_count
+            order.total_price = total_price
+            order.save()
+
+        except Exception as e:
+            transaction.savepoint_rollback(save_id)
+            return JsonResponse({'res':7, 'errmsg':'下单失败'})
+
+        transaction.savepoint_commit(save_id)
+
+        conn.hdel(cart_key, *sku_ids)
+
+        return JsonResponse({'res':5, 'message':'创建成功'})
